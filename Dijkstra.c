6559def9fad4d828d5110fe5605669046560de46
maanya-chawla <maanyachawla.cs24@bmsce.ac.in>
2026-06-02 20:19:29 +0530

    Add files via upload

---
+++ Dijkstra.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#define MAX 100
+#define INF 99999
+
+int minDist(int dist[], int visited[], int n) {
+    int min = INF, index = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i] && dist[i] < min) {
+            min = dist[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void dijkstra(int graph[MAX][MAX], int n, int src) {
+    int dist[MAX], visited[MAX];
+
+    
+    for (int i = 0; i < n; i++) {
+        dist[i] = INF;
+        visited[i] = 0;
+    }
+    dist[src] = 0;
+
+    for (int i = 0; i < n-1; i++) {
+        int u = minDist(dist, visited, n);
+        visited[u] = 1;
+
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && graph[u][v] &&
+                dist[u] + graph[u][v] < dist[v]) {
+
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    
+    printf("\nVertex\tDistance from Source\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\t%d\n", i, dist[i]);
+}
+
+int main() {
+    int n, src;
+    int graph[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix (0 for no edge):\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &graph[i][j]);
+
+    printf("Enter source vertex: ");
+    scanf("%d", &src);
+
+    dijkstra(graph, n, src);
+
+    return 0;
+}
